8d2743c1fd45a5fcfa8878fd6178ab898956b99a
enrikerf <enrike9066@gmail.com>
2022-01-13 13:13:22 +0100

    command executor wireframe

--- commandExecutor/app/Adapter/In/ApiGrcp/Controller/CallController.go
+++ commandExecutor/app/Adapter/In/ApiGrcp/Controller/CallController.go
@@ -2,7 +2,10 @@ package Controller
 
 import (
 	"context"
+	"fmt"
 	"github.com/Enrikerf/pfm/commandExecutor/app/Adapter/In/ApiGrcp/gen/call"
+	"os/exec"
+	"strings"
 )
 
 type CallController struct {
@@ -10,8 +13,21 @@ type CallController struct {
 }
 
 func (s CallController) CallUnary(ctx context.Context, request *call.CallRequest) (*call.CallResponse, error) {
-	println("unary")
-	return &call.CallResponse{Result: "result"}, nil
+	var resultContent string
+	parts := strings.Fields(request.Command)
+	head := parts[0]
+	// Head at this point is "sudo"
+	parts = parts[1:len(parts)]
+	cmd := exec.Command(head, parts...)
+	stdout, err := cmd.Output()
+	if err != nil {
+		resultContent = err.Error()
+		fmt.Println(err.Error())
+	} else {
+		resultContent = string(stdout)
+	}
+
+	return &call.CallResponse{Result: resultContent}, nil
 }
 
 func (s CallController) CallServerStream(request *call.CallRequest, server call.CallService_CallServerStreamServer) error {
